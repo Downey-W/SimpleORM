7b47dfe38eb06cc36013908b5d7fcff7d7dc7a2a
Downey-W <1010954504@qq.com>
2015-06-16 20:47:53 +0800

    third commit

--- SimpleORM/README.md
+++ SimpleORM/README.md
@@ -1,4 +1,4 @@
 # SimpleORM
 A SimpleORM support CRUD.  It is great honor for me to get your  advice. Thank you!
 #test
-发达算法山东
+范德萨发大水发
--- SimpleORM/src/cn/out/wwl/exception/NoTableIdException.java
+++
@@ -1,9 +0,0 @@
-package cn.out.wwl.exception;
-
-public class NoTableIdException extends Exception{
-	
-	public NoTableIdException()
-	{
-		super("There is no @TableId in your POJO when you want to update an object");
-	}
-}
